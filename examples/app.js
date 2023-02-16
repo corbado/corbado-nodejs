9a1b3b08fff562342c461c18b8b350ef88f979bf
vincentdelitz <vincent.delitz@tum.de>
2023-02-16 22:14:30 +0100

    improved example

--- examples/app.js
+++ examples/app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const Corbado = require('node-sdk');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3005;
+const PROJECT_ID = process.env.PROJECT_ID;
+const API_SECRET = process.env.API_SECRET;
 
 
 app.use(cors());
@@ -13,32 +18,37 @@ app.get('/', function (req, res) {
 });
 
 
-app.get('/logged-in', async function (req, res) {
+app.get('/logged-in', async function (req, res, next) {
+    const corbado = new Corbado(PROJECT_ID, API_SECRET);
+    const token = req.query.sessionToken;
+    const clientInfo = corbado.utils.getClientInfo(req);
 
-    const corbado = new Corbado('pro-15532438046016049903','dyJACyayAB8boJFPxtuVNCgb3HUo2f');
+    try {
+        const {data} = await corbado.sessionService.sessionTokenVerify(token, clientInfo);
+        const userData = JSON.parse(data.userData);
 
-    let token = req.query.sessionToken;
-    let clientInfo = corbado.utils.getClientInfo(req);
+        /*
+          IMPLEMENT YOUR SESSION MANAGEMENT HERE
+        */
 
-    let {data} = await corbado.sessionService.sessionTokenVerify(token, clientInfo);
-
-    console.log(data)
-    let userData = JSON.parse(data.userData);
-
-    /*
-        IMPLEMENT YOUR SESSION MANAGEMENT HERE
-     */
-
-    res.json({
-        message: "The user " + userData.username + " has successfully logged in",
-        result: "success"
-    });
-})
-
-
-app.listen(3005, function () {
-    console.log('Example app listening on port 3005!');
+        res.json({
+            message: "The user " + userData.username + " has successfully logged in",
+            result: "success"
+        });
+    } catch (error) {
+        console.error('Error verifying session token:', error.message);
+        next(error);
+    }
 });
 
+app.use((err, req, res, next) => {
+    console.error('Unhandled error:', err.message);
+    res.status(500).json({
+        message: 'Internal server error',
+        result: 'error'
+    });
+});
 
-
+app.listen(PORT, function () {
+    console.log(`Example app listening on port ${PORT}!`);
+});
