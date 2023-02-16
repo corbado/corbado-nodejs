97db60a6f8cf7c56ea4b82d97090b5292963171d
vincentdelitz <vincent.delitz@tum.de>
2023-02-16 19:28:09 +0100

    restructured existing services

---
+++ examples/app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const Corbado = require('node-sdk');
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+
+app.get('/logged-in', async function (req, res) {
+
+    const corbado = new Corbado('dyJACyayAB8boJFPxtuVNCgb3HUo2f', {
+        'projectID': 'pro-15532438046016049903',
+        'origin': 'http://localhost:3000',
+    }); //TODO origin must be optional
+
+    let token = req.query.sessionToken;
+    let clientInfo = corbado.utils.getClientInfo(req);
+
+    let {data} = await corbado.sessionService.sessionTokenVerify(token, clientInfo);
+
+    console.log(data)
+    let userData = JSON.parse(data.userData);
+
+    /*
+        IMPLEMENT YOUR SESSION MANAGEMENT HERE
+     */
+
+    res.json({
+        message: "The user " + userData.username + " has successfully logged in",
+        result: "success"
+    });
+})
+
+
+app.listen(3005, function () {
+    console.log('Example app listening on port 3005!');
+});
+
+
+
