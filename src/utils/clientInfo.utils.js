2adcca804cf5b00da556bb002f5e658668d197e9
Vincent Delitz <vincent.delitz@tum.de>
2023-04-27 18:32:54 +0200

    only one IP address is used as remote address if proxy in place

--- src/utils/clientInfo.utils.js
+++ src/utils/clientInfo.utils.js
@@ -1,7 +1,25 @@
 module.exports = getClientInfo = (req) => {
     return {
-        remoteAddress: req.headers['x-forwarded-for'] || req.socket.remoteAddress,
+        remoteAddress: getRemoteAddress(req),
         userAgent: req.get('user-agent'),
         origin: req.get('origin')
     };
 };
+
+function getRemoteAddress (req) {
+    const forwardedFor = req.headers['x-forwarded-for'];
+
+    if (forwardedFor) {
+        if (Array.isArray(forwardedFor)) {
+            // If 'x-forwarded-for' header is an array, use the first IP address in the array
+            return forwardedFor[0].trim();
+        } else {
+            // If 'x-forwarded-for' header is a string, split it by comma and return the first IP address
+            return forwardedFor.split(',')[0].trim();
+        }
+    }
+
+    // Otherwise, return the IP address from the remote socket
+    return req.socket.remoteAddress ?? '';
+}
+
