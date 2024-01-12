8814211032df2ad7f34afbae01e4022d9e2e7317
Sam Odum <sam.odum@corbado.com>
2024-01-12 16:42:28 +0100

    🔧 Setup test env

--- tests/setupJest.template.js
+++ tests/setupJest.template.js
@@ -1,5 +1,5 @@
 process.env.NODE_ENV = 'local';
 process.env.APP_VERSION = 'v0.1';
-process.env.PROJECT_ID = 'pro-6062510865197459469';
-process.env.API_SECRET = 'corbado1_oNfJQ2UFUZMnegtwm8AnewNRyHXkNd';
+process.env.PROJECT_ID = 'Your-Credentials-Here';
+process.env.API_SECRET = 'Your-Credentials-Here';
 process.env.BACKEND_API_URL = 'https://backendapi.corbado.io';
