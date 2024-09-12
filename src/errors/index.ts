fb4ccd6c87b0801e9e7baf678e186d3e3b97da18
Dopeamin <mohamed.amine.hamdouni@corbado.com>
2024-09-12 13:26:13 +0200

    fixes export

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,6 +1,6 @@
 import BaseError from './baseError.js';
 import ServerError, { ErrorDetails, RequestData, ServerErrorType } from './serverError.js';
-import JWTError from './validationError.js';
+import ValidationError from './validationError.js';
 import httpStatusCodes from './httpStatusCodes.js';
 
-export { BaseError, ServerError, httpStatusCodes, ErrorDetails, RequestData, ServerErrorType, JWTError };
+export { BaseError, ServerError, httpStatusCodes, ErrorDetails, RequestData, ServerErrorType, ValidationError };
